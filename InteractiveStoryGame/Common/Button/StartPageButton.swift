f97c978150e21ca97aa4f9a09ba75909ac274312
Puffsworld <aidenpearce8306@gmail.com>
2025-07-29 16:30:06 -0700

    Implement StartPageButton and StartPageButtonModel with customizable properties

--- InteractiveStoryGame/Common/Button/StartPageButton.swift
+++ InteractiveStoryGame/Common/Button/StartPageButton.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct StartPageButton: View {
+    let model: StartPageButtonModel
+
+    init(model: StartPageButtonModel) {
+        self.model = model
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: model.action) {
+            Text(model.name)
+                .font(.system(size: 18, weight: .medium))
+                .foregroundColor(.white)
+                .padding(EdgeInsets(top: 12, leading: 24, bottom: 12, trailing: 24))
+                .background(model.backgroundColor)
+                .cornerRadius(model.cornerRadius ?? 8)
+        }
     }
 }
 
 #Preview {
-    StartPageButton()
+    VStack(spacing: 20) {
+        // Default button
+        StartPageButton(
+            model: StartPageButtonModel(
+                name: "Start Game",
+                action: { print("Start Game tapped") }
+            )).padding()
+
+        // Custom corner radius
+        StartPageButton(
+            model: StartPageButtonModel(
+                name: "Settings",
+                action: { print("Settings tapped") },
+                cornerRadius: 20,
+                backgroundColor: .green
+            ))
+    }
+    .padding()
+    StartPageButton(model: StartPageButtonModel(name: "About US", action:{print("click about us")}, cornerRadius: 20))
+        .padding()
+        
 }
--- InteractiveStoryGame/Common/Button/StartPageButtonModel.swift
+++ InteractiveStoryGame/Common/Button/StartPageButtonModel.swift
@@ -5,4 +5,23 @@
 //  Created by wentao li on 7/25/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct StartPageButtonModel {
+    let name: String
+    let action: () -> Void
+    var cornerRadius: CGFloat?
+    var backgroundColor: Color
+    
+    init(
+        name: String,
+        action: @escaping () -> Void,
+        cornerRadius: CGFloat? = nil,
+        backgroundColor: Color = .blue
+    ) {
+        self.name = name
+        self.action = action
+        self.cornerRadius = cornerRadius
+        self.backgroundColor = backgroundColor
+    }
+}
