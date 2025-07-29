7990053b81e4093149b4162b4b49a3a0d8b2dac5
Puffsworld <aidenpearce8306@gmail.com>
2025-07-29 16:26:53 -0700

    Add StartPageButton and StartPageButtonModel structures

---
+++ InteractiveStoryGame/Common/Button/StartPageButton.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  InteractiveStoryGame
+//
+//  Created by wentao li on 7/25/25.
+//
+
+import SwiftUI
+
+struct StartPageButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartPageButton()
+}
---
+++ InteractiveStoryGame/Common/Button/StartPageButtonModel.swift
@@ -0,0 +1,8 @@
+//
+//  StartPageButtonModel.swift
+//  InteractiveStoryGame
+//
+//  Created by wentao li on 7/25/25.
+//
+
+import Foundation
